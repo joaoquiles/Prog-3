a75e3eef496a3ee21cef2e358cf42c778aef0d3d
joaoquiles <42985671+joaoquiles@users.noreply.github.com>
2019-04-03 20:31:11 -0300

    Add files via upload

---
+++ Aula/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+	</head>
+	<body>
+		<?php
+			for ($i=0; $i <100 ; $i++) { 
+				echo "Ola $i <br>";
+			}
+			
+		?>
+
+		<script>
+			for (var i = 0 - 1; i < 100; i++) {
+				document.write("Ola mundo "+i+" <br>");
+			}
+		</script>
+	</body>
+</html>
