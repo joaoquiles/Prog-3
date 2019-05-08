3bc67159c70dcdb6d1e67b655e4ad365059f0e11
João Victor Santana Quiles <42985671+joaoquiles@users.noreply.github.com>
2019-05-08 19:23:16 -0300

    Add files via upload

---
+++ prog3/insta/registro.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>InstaClone - Registro</title>
+    <link rel="stylesheet" href="css/estyle.css">
+  </head>
+  <body>
+      <main>
+        <section>
+          <header>
+            <h1>InstaClone</h1>
+            <h3>Cadastre-se... </h3>
+          </header>
+          <div>
+            <form>
+              <div>
+                <input type="text" name="email" placeholder="Seu e-mail">
+              </div>
+              <div>
+                <input type="text" name="nome" placeholder="Seu nome">
+              </div>
+              <div>
+                <input type="text" name="usuario" placeholder="Seu nome de usuario">
+              </div>
+              <div>
+                <input type="password" name="senha" placeholder="Sua senha">
+              </div>
+              <div>
+                <input type="checkbox" name="concordo">
+                <label for="concordo"></label>
+              </div>
+              <div>
+                <input type="submit" value="Cadastre-se">
+                <input type="reset">
+              </div>
+            </form>
+          </div>
+          <footer>
+            <h3>Ja tem uma conta ?</h3>
+            <a href="login.php">Conecte-se</a>
+          </footer>
+        </section>
+      </main>
+  </body>
+</html>
